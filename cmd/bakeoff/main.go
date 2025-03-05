b82494d943af7294591fd0ab3415090b598c9703
Robert Curry <robertcurry0216@gmail.com>
2025-03-05 12:12:56 +1100

    feat: Add user route with HTML and JSON support and template rendering

--- cmd/bakeoff/main.go
+++ cmd/bakeoff/main.go
@@ -4,26 +4,29 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/robertcurry0216/bakeoff/internal/helpers"
 )
 
 var db = make(map[string]string)
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
+	r.LoadHTMLGlob("templates/*")
 
 	// Ping test
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "pong")
 	})
 
-	// Get user value
-	r.GET("/user/:name", func(c *gin.Context) {
-		user := c.Params.ByName("name")
-		value, ok := db[user]
-		if ok {
-			c.JSON(http.StatusOK, gin.H{"user": user, "value": value})
-		} else {
-			c.JSON(http.StatusOK, gin.H{"user": user, "status": "no value"})
+	r.GET("/user/:email", func(c *gin.Context) {
+		email := c.Params.ByName("email")
+		data := helpers.GetUserAttributesFromEmail(email)
+
+		switch c.ContentType() {
+		case "application/json":
+			c.JSON(http.StatusOK, data)
+		default:
+			c.HTML(http.StatusOK, "basic.tmpl", gin.H{"title": "hi"})
 		}
 	})
 
